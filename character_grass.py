b975fe6c7d8e1fc095c01d6a6893796047452f46
jaehyeokim0921 <qoxmaos921@naver.com>
2024-09-24 12:27:20 +0900

    사각운동 기본 흐름 구조 완성

--- character_grass.py
+++ character_grass.py
@@ -21,12 +21,33 @@ def run_circle():
         boy.draw_now(x, y)  
         delay(0.1)
 
+def run_top():
+    print('TOP')
+    pass
+
+def run_right():
+    print('RIGHT')
+    pass
+
+def run_bottom():
+    print('BOTTOM')
+    pass
+
+def run_left():
+    print('LEFT')
+    pass
+
+
 def run_rectangle():
     print('Rectangle')
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
     pass
 
 while True:
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
 
