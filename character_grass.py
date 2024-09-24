6592658112d35b37ce339671f02795e985159e9b
jaehyeokim0921 <qoxmaos921@naver.com>
2024-09-24 11:56:16 +0900

    캐릭터 화면 출력 구현

--- character_grass.py
+++ character_grass.py
@@ -7,8 +7,16 @@ boy = load_image('character.png')
 
 def run_circle():
     print('Circle') # 내용이 전혀 없는 빈 함수
+
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
+
+    
+    pass
 def run_rectangle():
     print('Rectangle')
+    pass
 
 
     
@@ -16,5 +24,6 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
+    break
 
 close_canvas()
